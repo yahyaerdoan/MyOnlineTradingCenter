eab8699bded3c132b7445113a48393a2940c5752
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-06-03 18:50:15 -0500

    Contraint updated.

--- CoreLayer/MyOnlineTradingCenter.ApplicationLayer/Abstractions/IGenericRepositories/IReadRepository.cs
+++ CoreLayer/MyOnlineTradingCenter.ApplicationLayer/Abstractions/IGenericRepositories/IReadRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyOnlineTradingCenter.DomainLayer.Concretions.Entities.CommonEntity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace MyOnlineTradingCenter.ApplicationLayer.Abstractions.Repositories
 {
-    public interface IReadRepository<T> : IRepository<T> where T : class
+    public interface IReadRepository<T> : IRepository<T> where T : BaseEntity
     {
         IQueryable<T> GetAll();
         IQueryable<T> GetWhere(Expression<Func<T, bool>> expression);
