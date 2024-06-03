6889ee9e4d0180009bb09ba3b7869eaa22dc0584
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-06-02 23:46:04 -0500

    I Repository interfaces developed.

---
+++ CoreLayer/MyOnlineTradingCenter.ApplicationLayer/Abstractions/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyOnlineTradingCenter.ApplicationLayer.Abstractions.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        DbSet<T> Table { get; }
+    }
+}
---
+++ CoreLayer/MyOnlineTradingCenter.ApplicationLayer/Abstractions/Repositories/IWriteRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyOnlineTradingCenter.ApplicationLayer.Abstractions.Repositories
+{
+    public interface IWriteRepository<T> : IRepository<T> where T : class
+    {
+        Task<bool> AddAsync(T entity);
+        Task<bool> AddAsync(List<T> entity);
+        bool Remove(T entity);
+        bool Remove(string id);
+        Task<bool>  UpdateAsync(T entity);
+    }
+}
