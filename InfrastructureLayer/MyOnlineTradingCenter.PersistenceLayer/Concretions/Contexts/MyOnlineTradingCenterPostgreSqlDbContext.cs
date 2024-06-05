45b23f18bda0a62c282b8eef1468cc3402093034
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-06-05 14:25:04 -0500

    Save Change Async Interceptor developed for creating and updating date.

--- InfrastructureLayer/MyOnlineTradingCenter.PersistenceLayer/Concretions/Contexts/MyOnlineTradingCenterPostgreSqlDbContext.cs
+++ InfrastructureLayer/MyOnlineTradingCenter.PersistenceLayer/Concretions/Contexts/MyOnlineTradingCenterPostgreSqlDbContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using MyOnlineTradingCenter.DomainLayer.Concretions.Entities.CommonEntity;
 using MyOnlineTradingCenter.DomainLayer.Concretions.Entities.Entities;
 using System;
 using System.Collections.Generic;
@@ -16,6 +18,26 @@ namespace MyOnlineTradingCenter.PersistenceLayer.Concretions.Contexts
 
         public DbSet<Product> Products  { get; set; }
         public DbSet<Order> Orders  { get; set; }
-        public DbSet<Customer> Customers  { get; set; }      
+        public DbSet<Customer> Customers  { get; set; }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var data = ChangeTracker
+                .Entries<BaseEntity>()
+                 .Where(e => e.State == EntityState.Added || e.State == EntityState.Modified);
+
+            var currentTime = DateTime.UtcNow;
+
+            foreach (var datum in data)
+            {
+                _ = datum.State switch
+                {
+                    EntityState.Added => datum.Entity.CreatedDate = currentTime,
+                    EntityState.Modified => datum.Entity.UpdatedDate = currentTime,
+                };
+            }
+
+            return await base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
--- PresentationLayer/MyOnlineTradingCenter.RestfulApplicationInterfaceLayer/Controllers/ProductsController.cs
+++ PresentationLayer/MyOnlineTradingCenter.RestfulApplicationInterfaceLayer/Controllers/ProductsController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MyOnlineTradingCenter.ApplicationLayer.Abstractions;
+using MyOnlineTradingCenter.ApplicationLayer.Abstractions.IRepositories.ICustomerRepositories;
+using MyOnlineTradingCenter.ApplicationLayer.Abstractions.IRepositories.IOrderRepositories;
 using MyOnlineTradingCenter.ApplicationLayer.Abstractions.IRepositories.IProductRepositories;
 using MyOnlineTradingCenter.DomainLayer.Concretions.Entities.Entities;
 
@@ -13,23 +15,38 @@ namespace MyOnlineTradingCenter.RestfulApplicationInterfaceLayer.Controllers
         readonly private IProductWriteRepository _productWriteRepository;
         readonly private IProductReadRepository _productReadRepository;
 
-        public ProductsController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository)
+        readonly private ICustomerWriteRepository _customerWriteRepository;
+        readonly private ICustomerReadRepository _customerReadRepository;
+
+        readonly private IOrderWriteRepository _orderWriteRepository;
+        readonly private IOrderReadRepository _orderReadRepository;
+
+        public ProductsController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository, IOrderWriteRepository orderWriteRepository)
         {
             _productWriteRepository = productWriteRepository;
             _productReadRepository = productReadRepository;
+            _orderWriteRepository = orderWriteRepository;
         }
 
         [HttpGet]
         public async Task Get()
         {
+            #region AddRangeAsync
             //await _productWriteRepository.AddRangeAsync(new()
             //{
             //    new(){Id = Guid.NewGuid(), Name="product 2", Description="p2", Price = 6, CreatedDate= DateTime.UtcNow, UpdatedDate= DateTime.UtcNow, Stock = 11, Status= true}
             //});
-            Product product = await _productReadRepository.GetByIdAsync("efe30fe6-2734-4cbd-a23c-c025ffe739a5", true);
-            product.Name = "yahya 3";
-            product.UpdatedDate = DateTime.UtcNow;
-            await _productWriteRepository.SaveAsync();
+            #endregion
+            #region AsNoTracking
+            //Product product = await _productReadRepository.GetByIdAsync("efe30fe6-2734-4cbd-a23c-c025ffe739a5", true);
+            //product.Name = "yahya 3";
+            //product.UpdatedDate = DateTime.UtcNow;
+            //await _productWriteRepository.SaveAsync();
+            #endregion
+
+            await _orderWriteRepository.AddAsync(new Order() {Address="7588 W Granville Ave", Description="New Product" });
+            await _orderWriteRepository.SaveAsync();
+
         }
         [HttpGet("id")]
         public async Task<IActionResult> GetTask(string id)
