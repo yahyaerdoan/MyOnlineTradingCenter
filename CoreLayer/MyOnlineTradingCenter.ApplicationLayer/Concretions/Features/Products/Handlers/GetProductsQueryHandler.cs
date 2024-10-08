67036eacdd4ef709326672bb2d90b222a344fa91
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-10-07 19:19:22 -0500

    feat: List products with images and implement URI retrieval for image paths
    - Used the include method to fetch products along with associated images.
    - Integrated IHttpContextAccessor and IConfiguration to construct URIs and URLs for image paths dynamically.
    - Enabled efficient product image listing with accurate URL configuration based on request context.

--- CoreLayer/MyOnlineTradingCenter.ApplicationLayer/Concretions/Features/Products/Handlers/GetProductsQueryHandler.cs
+++ CoreLayer/MyOnlineTradingCenter.ApplicationLayer/Concretions/Features/Products/Handlers/GetProductsQueryHandler.cs
@@ -1,5 +1,7 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using MyOnlineTradingCenter.ApplicationLayer.Abstractions.IRepositories.IProductRepositories;
 using MyOnlineTradingCenter.ApplicationLayer.Concretions.Features.Products.Queries.Get;
@@ -16,22 +18,30 @@ public class GetProductsQueryHandler : IRequestHandler<GetProductsQueryRequest, 
 {
     private readonly IProductReadRepository _productReadRepository;
     private readonly ILogger<GetProductsQueryHandler> _logger;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly IConfiguration _configuration;
 
-    public GetProductsQueryHandler(IProductReadRepository productReadRepository, ILogger<GetProductsQueryHandler> logger)
+    public GetProductsQueryHandler(IProductReadRepository productReadRepository, ILogger<GetProductsQueryHandler> logger, IHttpContextAccessor httpContextAccessor, IConfiguration configuration)
     {
         _productReadRepository = productReadRepository;
         _logger = logger;
+        _httpContextAccessor = httpContextAccessor;
+        _configuration = configuration;
     }
 
     public async Task<GetProductsQueryResponse> Handle(GetProductsQueryRequest request, CancellationToken cancellationToken)
     {
+        var httpContext = _httpContextAccessor.HttpContext;
+        var requestScheme = httpContext.Request.Scheme;
+        var requestHost = httpContext.Request.Host.Value;
+
         var query = _productReadRepository.GetAll(false).OrderBy(p => p.Price);
 
         var totalProductCount = await query.CountAsync(cancellationToken);
 
         var products = await query
             .Skip((request.Pagination.Page) * request.Pagination.Size)
-            .Take(request.Pagination.Size)
+            .Take(request.Pagination.Size).Include(product => product.ImageFiles)
             .Select(p => new
             {
                 Id = p.Id,
@@ -41,6 +51,16 @@ public class GetProductsQueryHandler : IRequestHandler<GetProductsQueryRequest, 
                 Stock = p.Stock,
                 Status = p.Status,
                 CreatedDate = p.CreatedDate,
+                ImageFiles = p.ImageFiles.Select(image => new
+                {
+                    image.Id,
+                    image.Name,
+                    image.CreatedDate,
+                    image.UpdatedDate,
+                    image.ShowcasePicture,
+                    Path = $"{requestScheme}://{requestHost}/{_configuration["LocalStorageOrigin"]}/{image.Name}"
+                }).ToList()
+
             })
             .ToListAsync(cancellationToken);
         _logger.LogInformation("Products have been listed!");
