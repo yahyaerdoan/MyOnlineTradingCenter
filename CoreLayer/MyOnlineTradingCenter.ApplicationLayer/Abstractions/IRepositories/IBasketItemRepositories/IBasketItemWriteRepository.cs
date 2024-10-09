013142d4d2785ff4f510b6eb9e0a0e7e511b6bf1
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-10-09 15:59:52 -0500

    chore: Create repository classes for new entities
    - Added repository class definitions for newly created entities.
    - Prepared structure for future implementation of data operations.

---
+++ CoreLayer/MyOnlineTradingCenter.ApplicationLayer/Abstractions/IRepositories/IBasketItemRepositories/IBasketItemWriteRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyOnlineTradingCenter.ApplicationLayer.Abstractions.IRepositories.IBasketItemRepositories
+{
+    internal interface IBasketItemWriteRepository
+    {
+    }
+}
---
+++ InfrastructureLayer/MyOnlineTradingCenter.PersistenceLayer/Concretions/Repositories/BasketRepositories/BasketWriteRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyOnlineTradingCenter.PersistenceLayer.Concretions.Repositories.BasketRepositories
+{
+    internal class BasketWriteRepository
+    {
+    }
+}
