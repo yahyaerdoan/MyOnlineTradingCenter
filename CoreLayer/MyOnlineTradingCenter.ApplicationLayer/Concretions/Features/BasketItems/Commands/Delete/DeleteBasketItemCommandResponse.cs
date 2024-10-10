9a36c2f9ac2c1ef065f3877e90b28e0f259fa79c
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-10-09 20:51:57 -0500

    feat: Create CQRS classes for BasketItem methods
    - Added command and query classes to implement CQRS pattern for BasketItem operations.
    - Structured commands and queries to handle basket item methods effectively.
    - Prepared foundation for organized and scalable data operations in the basket service.

---
+++ CoreLayer/MyOnlineTradingCenter.ApplicationLayer/Concretions/Features/BasketItems/Commands/Delete/DeleteBasketItemCommandResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyOnlineTradingCenter.ApplicationLayer.Concretions.Features.BasketItems.Commands.Delete
+{
+    internal class DeleteBasketItemCommandResponse
+    {
+    }
+}
