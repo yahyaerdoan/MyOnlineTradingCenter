25d8c15dd0bf1d7a4684a341a82e9b4669c8c7da
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-10-10 00:04:56 -0500

    feat: Develop controller for BasketItem methods
    - Implemented controller endpoints to manage BasketItem operations.
    - Integrated CQRS methods to handle basket item actions through API routes.
    - Enabled organized access to BasketItem functionality for improved user interaction.

---
+++ PresentationLayer/MyOnlineTradingCenter.RestfulApplicationInterfaceLayer/Controllers/BasketItemsController.cs
@@ -0,0 +1,49 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyOnlineTradingCenter.ApplicationLayer.Concretions.Features.BasketItems.Commands.Create;
+using MyOnlineTradingCenter.ApplicationLayer.Concretions.Features.BasketItems.Commands.Delete;
+using MyOnlineTradingCenter.ApplicationLayer.Concretions.Features.BasketItems.Commands.Update;
+using MyOnlineTradingCenter.ApplicationLayer.Concretions.Features.BasketItems.Queries.Get;
+
+namespace MyOnlineTradingCenter.RestfulApplicationInterfaceLayer.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize(AuthenticationSchemes = "Admin")]
+public class BasketItemsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public BasketItemsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    [HttpGet]
+    public async Task<IActionResult> GetBasketItems([FromQuery] GetBasketItemsQueryRequest request)
+    {
+        List<GetBasketItemsQueryResponse> response = await _mediator.Send(request);
+        return Ok(response);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddBasketItem(AddBasketItemCommandRequest request)
+    {
+       AddBasketItemCommandResponse response = await _mediator.Send(request);
+        return Ok(response);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateBasketItemQuantity(UpdateBasketItemCommandRequest request)
+    {
+        UpdateBasketItemCommandResponse response = await _mediator.Send(request);
+        return Ok(response);
+    }
+
+    [HttpDelete("BasketItemId")]
+    public async Task<IActionResult> DeleteBasketItemQuantity([FromRoute] DeleteBasketItemCommandRequest request)
+    {
+        DeleteBasketItemCommandResponse response = await _mediator.Send(request);
+        return Ok(response);
+    }
+}
