62e5f3f8a20d7c73669b11a351de8d48527c5c57
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-10-15 21:44:28 -0500

    feat: Create CQRS classes for OrderItem management
    - Added command and query classes to implement CQRS pattern for OrderItem operations.
    - Organized OrderItem actions for improved separation of concerns and maintainability.
    - Established a structured foundation for efficient order item data handling.

---
+++ CoreLayer/MyOnlineTradingCenter.ApplicationLayer/Concretions/Features/OrderItems/Commands/Update/UpdateOrderItemCommandResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyOnlineTradingCenter.ApplicationLayer.Concretions.Features.OrderItems.Commands.Update
+{
+    internal class UpdateOrderItemCommandResponse
+    {
+    }
+}
---
+++ CoreLayer/MyOnlineTradingCenter.ApplicationLayer/Concretions/Features/OrderItems/Handlers/GetByIdOrderItemQueryHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyOnlineTradingCenter.ApplicationLayer.Concretions.Features.OrderItems.Handlers
+{
+    internal class GetByIdOrderItemQueryHandler
+    {
+    }
+}
