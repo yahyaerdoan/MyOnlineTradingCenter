2be30bd7d4e63cd3791f4f22cd272a875e84592a
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-10-15 20:57:58 -0500

    feat: Create CQRS classes for Order management
    - Added command and query classes to implement CQRS pattern for Order operations.
    - Structured Order-related actions for improved separation of concerns and scalability.
    - Prepared foundation for organized and efficient order data handling.

---
+++ CoreLayer/MyOnlineTradingCenter.ApplicationLayer/Concretions/Features/Orders/Handlers/GetOrdersQueryHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyOnlineTradingCenter.ApplicationLayer.Concretions.Features.Orders.Handlers
+{
+    internal class GetOrdersQueryHandler
+    {
+    }
+}
