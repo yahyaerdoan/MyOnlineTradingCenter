21ae84f5cee4a1204ba48bd23bb3ded541066660
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-12-07 01:30:07 -0600

    feat: Retrieve user info and send completed order emails - Implemented GetWhere method to fetch user info based on specific conditions. - Filled out SendCompletedOrderAsync method logic to send successful completion emails. - Ensures that mail is delivered reliably and as intended.

--- InfrastructureLayer/MyOnlineTradingCenter.PersistenceLayer/Concretions/Services/CompletedOrderService.cs
+++ InfrastructureLayer/MyOnlineTradingCenter.PersistenceLayer/Concretions/Services/CompletedOrderService.cs
@@ -1,4 +1,5 @@
-﻿using MyOnlineTradingCenter.ApplicationLayer.Abstractions.IRepositories.ICompletedOrderRepositories;
+﻿using Microsoft.EntityFrameworkCore;
+using MyOnlineTradingCenter.ApplicationLayer.Abstractions.IRepositories.ICompletedOrderRepositories;
 using MyOnlineTradingCenter.ApplicationLayer.Abstractions.IRepositories.IOrderRepositories;
 using MyOnlineTradingCenter.ApplicationLayer.Abstractions.IServices;
 using MyOnlineTradingCenter.DataTransferObjectLayer.Concretions.DataTransferObjects.CompletedOrders;
@@ -10,23 +11,34 @@ public class CompletedOrderService : ICompletedOrderService
 {
     private readonly ICompletedOrderWriteRepository _completedOrderWriteRepository;
     private readonly IOrderReadRepository _orderReadRepository;
+    private readonly IEmailService _emailService;
 
-    public CompletedOrderService(ICompletedOrderWriteRepository completedOrderWriteRepository, IOrderReadRepository orderReadRepository)
+    public CompletedOrderService(ICompletedOrderWriteRepository completedOrderWriteRepository, IOrderReadRepository orderReadRepository, IEmailService emailService)
     {
         _completedOrderWriteRepository = completedOrderWriteRepository;
         _orderReadRepository = orderReadRepository;
+        _emailService = emailService;
     }
 
     public async Task<bool> CompleteOrderAsync(string orderId)
     {
-        var order = await _orderReadRepository.GetByIdAsync(orderId);
-        if (order == null) return false;
+        var order = await _orderReadRepository.GetWhere(x => x.Id == Guid.Parse(orderId.ToString())).Select(x => new
+        {
+            x.OrderNumber,
+            x.CreatedDate,
+            UserEmail = x.User.Email,
+            UserFullName = x.User.FirstName + " " + x.User.LastName,
+        }).FirstOrDefaultAsync();
+
+        if (order == null || string.IsNullOrEmpty(order.UserEmail)) return false;
 
         var completedOrderDto = new CompleteOrderDto { OrderId = orderId };
         var completedOrder = new CompletedOrder() { OrderId = Guid.Parse(completedOrderDto.OrderId), Status = true };
 
         await _completedOrderWriteRepository.AddAsync(completedOrder);
         var savedResult = await _completedOrderWriteRepository.SaveAsync();
+
+        await _emailService.SendCompletedOrderAsync(order.UserEmail, order.OrderNumber, order.CreatedDate, order.UserFullName);
 
         return savedResult > 0;
     }
