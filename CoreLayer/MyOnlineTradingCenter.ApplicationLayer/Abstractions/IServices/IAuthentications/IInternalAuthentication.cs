2a74a8e4d9bd37d0484ea6a76fb4c478ac2edf69
Yahya Erdogan <yahyaerdoan@gmail.com>
2025-02-24 16:20:06 -0600

    chore: Remove unused namespaces - Cleaned up unused namespaces to improve code readability and maintainability.

--- CoreLayer/MyOnlineTradingCenter.ApplicationLayer/Abstractions/IServices/IAuthentications/IInternalAuthentication.cs
+++ CoreLayer/MyOnlineTradingCenter.ApplicationLayer/Abstractions/IServices/IAuthentications/IInternalAuthentication.cs
@@ -1,12 +1,6 @@
 ﻿using MyOnlineTradingCenter.ApplicationLayer.Concretions.Features.Users.LogInUsers.Commands.Create;
 using MyOnlineTradingCenter.ApplicationLayer.Concretions.Features.Users.RefreshTokenLogIns.Commands.Create;
 using MyOnlineTradingCenter.ApplicationLayer.Concretions.Responses;
-using MyOnlineTradingCenter.DataTransferObjectLayer.Concretions.DataTransferObjects.Tokens;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyOnlineTradingCenter.ApplicationLayer.Abstractions.IServices.IAuthentications;
 
